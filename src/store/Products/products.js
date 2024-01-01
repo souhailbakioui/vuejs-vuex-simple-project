8e40f92fe6a2b2f70cd403aea01ad5a2d00a3293
souhailbakioui <bakiouisohail@gmail.com>
2024-01-01 18:24:07 +0100

    use vuex Helpers mapState, mapActions, mapMutations, mapGetters

--- src/store/Products/products.js
+++ src/store/Products/products.js
@@ -8,10 +8,14 @@ export default {
         getProduct(state) {
           return state.products;
         },
+        getFirstProduct(state){
+          return state.products[0]
+        }
       },
       mutations: {
         
-        getProduct(state, products) {
+        setProduct(state, products) {
+          console.log("setProduct with Data",products)
           state.products=products
         }
       },
@@ -19,7 +23,7 @@ export default {
         async getProductApi(context) {
           await fetch("https://dummyjson.com/products")
             .then((res) => res.json())
-            .then(data=>context.commit('getProduct',data.products));
+            .then(data=>context.commit('setProduct',data.products));
         },
       },
 
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,18 +1,17 @@
 <template>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <button type="button" class="btn btn-success" @click="this.$store.dispatch('getProductApi')">Get Products</button>
+    <button type="button" class="btn btn-success" @click="getProductApi()">Get Products</button>
     <section style="background-color: #eee;">
         <div class="container py-5">
-            <div v-for="product in this.$store.getters.getProduct" :key="product" class="row justify-content-center mb-3">
+            <div v-for="product in this.ProductModule.products" :key="product" class="row justify-content-center mb-3">
                 <div class="col-md-12 col-xl-10">
                     <div class="card shadow-0 border rounded-3">
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
                                     <div class="bg-image hover-zoom ripple rounded ripple-surface">
-                                        <img :src=" product?.thumbnail"
-                                            class="w-100" />
+                                        <img :src="product?.thumbnail" class="w-100" />
                                         <a href="#!">
                                             <div class="hover-overlay">
                                                 <div class="mask" style="background-color: rgba(253, 253, 253, 0.15);">
@@ -30,7 +29,7 @@
                                             <i class="fa fa-star"></i>
                                             <i class="fa fa-star"></i>
                                         </div>
-                                        
+
                                     </div>
                                     <div class="mt-1 mb-0 text-muted small">
                                         <span>100% cotton</span>
@@ -70,12 +69,32 @@
             </div>
 
         </div>
+        <!-- For Display State of Product Module {{this.ProductModule.products }} -->
+        <!-- this for display  a getter in Product Module {{ getFirstProduct }} -->
     </section>
 </template>
 
 <script>
+import { mapState, mapActions, mapMutations, mapGetters } from 'vuex';
 export default {
+    computed: {
+        ...mapState(['ProductModule']),  //va a buscar el estado de product en el store y lo va a poner como propiedad
+        ...mapGetters(['getFirstProduct'])
+    },
+    methods: {
+        ...mapActions(['getProductApi']),
+        ...mapMutations(['setProduct']),
+        async getProductData() {
+            console.log('goo')
 
+            await this.$store.dispatch('getProductApi')  // dispatching the action to get data from backend and store it in state
+            setTimeout(() => {
+                this.setProduct([])
+
+            }, 1000)
+            console.log('DONE')
+        }
+    }
 }
 </script>
 
