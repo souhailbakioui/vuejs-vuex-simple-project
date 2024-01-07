9927bebe6ac61d5cf6e84b19f3fba570c19b35ad
souhailbakioui <bakiouisohail@gmail.com>
2024-01-07 21:39:46 +0100

    add strict mode

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import { createStore } from "vuex";
 import ProductModule from "@/store/Products/products";
 import UserModule from "@/store/User/user";
 export default createStore({
+  strict:true,
   state: {},
   getters: {},
   mutations: {},
